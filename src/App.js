184a923420386487dbf72f684a37c59cedf78e99
tiwsashank <tiwcshashank@gmail.com>
2023-06-14 22:26:57 -0400

    1-component-introduction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <h1>Hello world!!</h1>
+  </div>
+    );
 }
 
 export default App;
