9556fb4a86db9eea96ebc30d1f83664aa5c0d522
tiwsashank <tiwcshashank@gmail.com>
2023-06-14 23:11:34 -0400

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
 
+//passing data via variable
+const title = 'React';
+
+// const myStyle={
+//   color:'white',
+//   backgroundColor: 'lightblue',
+//   borderstyle: '2px solid black'
+// };
+
+//passing data from function
+function getTitle(testvar){
+  return testvar
+}
+
+
 function App() {
   return (
   <div>
-    <h1>Hello world!!</h1>
-  </div>
+      <h1>Testing data from variable: {title}</h1>
+      <h1>Testing data from function: {getTitle('world')}</h1>
+
+    <label htmlFor='search'>Search: </label>
+    <input id="search" type="text" />
+   
+    </div>
     );
 }
+
+
 
 export default App;
